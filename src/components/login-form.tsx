a318693d4222b2a8bb88f8b242b40768f5987399
한동현 <hando1220@ajou.ac.kr>
2025-02-28 14:23:21 +0900

    feat: 로그인 페이지 추가

---
+++ src/components/login-form.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">로그인</CardTitle>
+          <CardDescription>아올다 통합 계정을 사용하여 로그인합니다</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">사용자 이름</Label>
+                <Input id="username" type="text" placeholder="사용자 이름" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">비밀번호</Label>
+                <Input id="password" type="password" placeholder="비밀번호" required />
+              </div>
+              <Button type="submit" className="w-full">
+                로그인
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              계정이 없으신가요?
+              <a href="#" className="ml-2 underline underline-offset-4">
+                아올다 프로젝트 신청하기
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router';
 import Root from '@/pages/Root';
 import Home from '@/pages/Home';
+import Login from '@/pages/Login';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Root />}>
         <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
