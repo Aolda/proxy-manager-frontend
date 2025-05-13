be02f2f4c8e2d1917f58a08b353ecd7bfaaf0ff7
한동현 <hando1220@ajou.ac.kr>
2025-05-13 15:43:53 +0900

    fix: 레이아웃 수정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -9,7 +9,7 @@ export default function Root() {
     <div className="[--header-height:calc(theme(spacing.14))]">
       <SidebarProvider className="flex flex-col">
         <SiteHeader />
-        <div className="flex flex-1">
+        <div className="flex flex-1 px-0 my-0 lg:mx-6">
           <AppSidebar />
           <SidebarInset className="overflow-hidden">
             <Outlet />
